f588e1ce905a0ca49682c2dd1cf2e1bb03a487e7
Brian Muse <brian@onemightyroar.com>
2013-04-22 13:38:23 -0400

    Adding init method to redid singleton. Connection is only created when the first client call is made.

--- src/Redis.php
+++ src/Redis.php
@@ -4,8 +4,8 @@
  *
  * Allows the easy use of queueing multiple Redis commands into a "transaction" to be executed at one time
  *
- * @copyright	2013 One Mighty Roar
- * @link	 	http://onemightyroar.com
+ * @copyright 2013 One Mighty Roar
+ * @link http://onemightyroar.com
  */
 
 namespace OneMightyRoar\PHP_Redis_Transaction_Queue;
@@ -37,79 +37,79 @@ class Redis extends Singleton
      * @access private
      */
     private $keys;
-	
-	/**
-	 * Constructor
+
+    /**
+     * Initialize the Redis singleton with the proper Redis connection config
      *
-     * @todo document params better
-     * @param array $connection_config
-     * @param array $key_store_keys
-	 * @access protected
+     * @param array $connection_config Connection credentials for Redis
+     * @param array $key_store_keys Array of values to build Redis keys with
      */
-    protected function __construct(array $connection_config, array $key_store_keys = array())
+    public function init(array $connection_config, array $key_store_keys = array())
     {
         $this->keys = $key_store_keys;
-
-		$this->client = new Client($connection_config);
-  		$this->client->connect();
-	}
-
-	/**
-	 * Getter for the client object
-	 *
-	 * @access public
-	 * @return \OneMightyRoar\PHP_Redis_Transaction_Queue\Client
-	 */
-	public function getClient()
-    {
-		return $this->client;
-	}
-
-	/**
-	 * Getter for the Redis keys config
-	 * 
-	 * @access public
-	 * @return array
-	 */
-	public function getKeys()
-    {
-		return $this->keys;
-	}
-
-	/**
-	 * Get the name of a Redis key
-	 *
-	 * Builds a redis key name intelligently by
-	 * adding in the ':' markers in different levels
-	 * 
-	 * @param string $key_name 
-	 * @access public
-	 * @return string
-	 */
-	public function getKey($key_name)
-    {
-		$master_key = $this->keys['master'];
-
-		// Grab our key based on our config
-		if (!in_array($key_name, $this->keys, true)) {
-			throw new DomainException('Redis key not pre-defined');
-		}
-
-		return $master_key . ':' . $this->keys[ $key_name ];
-	}
-
-	/**
-	 * Magic method for all calls. Passes the call through to the client
-	 *
-	 * @access public
-	 * @param string $method
-	 * @param array $arguments
-	 * @return mixed
-	 */
-    public function __call($method, $arguments = array())
-    {
-    	// Pass the call through to the actual client
-    	return call_user_func_array(array($this->client, $method), $arguments);
+        $this->client = new Client($connection_config);
     }
 
-} // End class Redis
+    /**
+     * Getter for the client object
+     *
+     * @access public
+     * @return \OneMightyRoar\PHP_Redis_Transaction_Queue\Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Getter for the Redis keys config
+     *
+     * @access public
+     * @return array
+     */
+    public function getKeys()
+    {
+        return $this->keys;
+    }
+
+    /**
+     * Get the name of a Redis key
+     *
+     * Builds a redis key name intelligently by
+     * adding in the ':' markers in different levels
+     *
+     * @param string $key_name
+     * @access public
+     * @return string
+     * @throws \DomainException
+     */
+    public function getKey($key_name)
+    {
+        $master_key = $this->keys['master'];
+
+        // Grab our key based on our config
+        if (!in_array($key_name, $this->keys, true)) {
+            throw new DomainException('Redis key not pre-defined');
+        }
+
+        return $master_key . ':' . $this->keys[ $key_name ];
+    }
+
+    /**
+     * Magic method for all calls. Passes the call through to the client
+     *
+     * @access public
+     * @param string $method
+     * @param array $arguments
+     * @return mixed
+     */
+    public function __call($method, $arguments = array())
+    {
+        if (!$this->client->isConnected()) {
+            $this->client->connect();
+        }
+
+        // Pass the call through to the actual client
+        return call_user_func_array(array($this->client, $method), $arguments);
+    }
+}
