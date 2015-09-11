0fdd9a33a44eb4139cc104b6cdf9f11e352b4282
Mark Costello <mark@markcostello.net>
2015-09-11 14:32:51 -0400

    First pass at testing the supported commands.

---
+++ tests/OneMightyRoar/PredisToolkit/Tests/ClientTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Predis-Toolkit
+ *
+ * Additional classes and functionality to extend Predis
+ *
+ * @copyright 2013 One Mighty Roar
+ * @link http://onemightyroar.com
+ */
+
+namespace OneMightyRoar\PredisToolkit\Test;
+
+use OneMightyRoar\PredisToolkit\Client;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Tests the {@link Client} class.
+ */
+class ClientTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Properties
+     */
+
+    /**
+     * The {@link Client} to test.
+     *
+     * @type Client
+     */
+    private $client;
+
+    /**
+     * Methods
+     */
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->client = new Client($this->getMockConnection());
+    }
+
+    private function getMockConnection()
+    {
+        $mock_connection = $this->getMock('Predis\Connection\ConnectionInterface');
+
+        return $mock_connection;
+    }
+
+    public function testSupportedCommands()
+    {
+        $profile = $this->client->getProfile();
+
+        $commands = $profile->getSupportedCommands();
+
+        $key = 'COMMAND:ARG';
+
+        foreach ($commands as $cmd => $cls) {
+            $command = null;
+
+            if ('info' === $cmd) {
+                $this->client->getConnection()->expects($this->at(0))->method('executeCommand')
+                    ->with($this->callback(function ($com) use (&$command) {
+                        $command = $com;
+                        return true;
+                    }))
+                    ->will($this->returnValue('INFO'));
+
+                $this->client->$cmd();
+                $this->assertInstanceOf($cls, $command);
+            } elseif ('quit' === $cmd) {
+                $this->client->getConnection()->expects($this->at(0))->method('disconnect')->will($this->returnValue(null));
+
+                $val = $this->client->$cmd($key);
+                $this->assertNull($val);
+            } else {
+                $this->client->getConnection()->expects($this->at(0))->method('executeCommand')
+                    ->with($this->callback(function ($com) use (&$command) {
+                        $command = $com;
+                        return true;
+                    }));
+
+                $this->client->$cmd($key, []);
+                $this->assertInstanceOf($cls, $command);
+            }
+        }
+    }
+}
